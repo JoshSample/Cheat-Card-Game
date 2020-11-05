eb5e745eccdb96b606800b8a9e4cf7c01d07b756
JoshSample <joshsample98@gmail.com>
2020-11-05 14:59:40 -0600

    implemented Player class

--- CheatCardGame/cheatcardgame/Player.java
+++ CheatCardGame/cheatcardgame/Player.java
@@ -1,5 +1,35 @@
 package cheatcardgame;
 
-public class Player {
+import java.io.Serializable;
 
+public class Player implements Serializable {
+	// Private data fields for the username and password.
+	private String username;
+	private String password;
+	
+	// constructor for player class
+	public Player(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	// setter for username
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	// getter for username
+	public String getUsername() {
+		return username;
+	}
+	
+	// setter for password
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	// getter for password
+	public String getPassword() {
+		return password;
+	}
 }
