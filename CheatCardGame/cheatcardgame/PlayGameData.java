4dffc4abfefa47af92e8b56d268fee53d0ddee73
Boa476 <45569169+Boa476@users.noreply.github.com>
2020-11-12 19:35:36 -0600

    modified PlayGameData

--- CheatCardGame/cheatcardgame/PlayGameData.java
+++ CheatCardGame/cheatcardgame/PlayGameData.java
@@ -1,14 +1,41 @@
 package cheatcardgame;
 
-public class PlayGameData {
-	private Player player;
-	private Player opponent;
+import java.io.Serializable;
+
+public class PlayGameData implements Serializable {
+	//private Player player;
+	//private Player opponent;
 	private int playerCount;
 	private int opponentCount;
+	private boolean turn;
+	private String playedCard;
 	
 	public PlayGameData(Player player, Player opponent) {
-		this.player = player;
-		this.opponent = opponent;
+		//this.player = player;
+		//this.opponent = opponent;
+	}
+	
+	public void setTurn(boolean val) {
+		this.turn = val;
+	}
+	
+	public boolean getTurn() {
+		return turn;
+	}
+	
+	public void setPlayerCount(int count) {
+		this.playerCount = count;
+	}
+	
+	public void setOpponentCount(int count) {
+		this.opponentCount = count;
+	}
+	
+	public void setPlayedCard(String playedCard) {
+		this.playedCard = playedCard;
+	}
+	public String getPlayedCard() {
+		return playedCard;
 	}
 	
 	
