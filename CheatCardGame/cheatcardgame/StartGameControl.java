438d20b800bbdcde549c236117295879d0737003
JoshSample <joshsample98@gmail.com>
2020-11-24 17:56:38 -0600

    Added some code to StartGame model view controller

--- CheatCardGame/cheatcardgame/StartGameControl.java
+++ CheatCardGame/cheatcardgame/StartGameControl.java
@@ -1,5 +1,24 @@
 package cheatcardgame;
 
-public class StartGameControl {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+public class StartGameControl implements ActionListener {
+	// private data fields
+	private JPanel container;
+	private CheatClient cheatClient;
+	
+	// constructor, sets container and client
+	public StartGameControl(JPanel container, CheatClient cheatClient) {
+		this.container = container;
+		this.cheatClient = cheatClient;
+	}
+	
+	public void actionPerformed(ActionEvent ae) {
+		
+		
+	}
 
 }
--- CheatCardGame/cheatcardgame/StartGamePanel.java
+++ CheatCardGame/cheatcardgame/StartGamePanel.java
@@ -1,5 +1,12 @@
 package cheatcardgame;
 
-public class StartGamePanel {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class StartGamePanel extends JPanel {
+	// data fields for GUI components
+	private JButton startGame;
+	private JButton quitGame;
 
 }
