7f8b33da2e498d2bae7a81ed1204c00342c31034
JoshSample <joshsample98@gmail.com>
2020-11-24 18:27:24 -0600

    Implemented StartGameControl and StartGamePanel

--- CheatCardGame/cheatcardgame/StartGameControl.java
+++ CheatCardGame/cheatcardgame/StartGameControl.java
@@ -3,22 +3,32 @@ package cheatcardgame;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-import java.io.IOException;
 
 public class StartGameControl implements ActionListener {
 	// private data fields
 	private JPanel container;
 	private CheatClient cheatClient;
-	
+
 	// constructor, sets container and client
 	public StartGameControl(JPanel container, CheatClient cheatClient) {
 		this.container = container;
 		this.cheatClient = cheatClient;
 	}
-	
+
 	public void actionPerformed(ActionEvent ae) {
-		
-		
+		// Get the name of the button clicked.
+		String command = ae.getActionCommand();
+
+		// The quit button will exit the user back to the initial panel
+		if (command.equals("Quit")) {
+			CardLayout cardLayout = (CardLayout)container.getLayout();
+			cardLayout.show(container, "1");
+		}
+		// Start Game button sends the user into the Play Game panel
+		else if (command.equals("Start Game")) {
+			CardLayout cardLayout = (CardLayout)container.getLayout();
+			cardLayout.show(container, "5");
+		}
 	}
 
 }
--- CheatCardGame/cheatcardgame/StartGamePanel.java
+++ CheatCardGame/cheatcardgame/StartGamePanel.java
@@ -5,8 +5,23 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class StartGamePanel extends JPanel {
-	// data fields for GUI components
-	private JButton startGame;
-	private JButton quitGame;
-
+	public StartGamePanel(StartGameControl sgc) {
+		// set the grid layout
+		setLayout(new GridLayout(2, 1, 100, 100));
+		
+		// title of game
+		JLabel lblCheatCardGame = new JLabel("Cheat Card Game", JLabel.CENTER);
+		lblCheatCardGame.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 38));
+		add(lblCheatCardGame);
+		
+		// create a button panel, add the buttons
+		JPanel buttonPanel = new JPanel();
+		add(buttonPanel);
+		JButton startButton = new JButton("Start Game");
+		buttonPanel.add(startButton);
+		startButton.addActionListener(sgc);
+		JButton quitButton = new JButton("Quit");
+		buttonPanel.add(quitButton);
+		quitButton.addActionListener(sgc);
+	}
 }
