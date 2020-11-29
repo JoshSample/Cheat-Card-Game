c588f975cbd89326082d618c12e2dae7e12aaf3d
JoshSample <joshsample98@gmail.com>
2020-11-29 16:49:33 -0600

    Cleaned up code for Database

--- CheatCardGame/cheatcardgame/Database.java
+++ CheatCardGame/cheatcardgame/Database.java
@@ -6,9 +6,7 @@ import java.sql.*;
 
 
 public class Database {
-
 	private Connection conn;
-	//Add any other data fields you like – at least a Connection object is mandatory
 
 	public Database() throws IOException {
 		//Add your code here
