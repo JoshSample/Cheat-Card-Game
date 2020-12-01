61d322826f8851b01ebe7c81038d78c0d69f7920
JoshSample <joshsample98@gmail.com>
2020-12-01 16:00:44 -0600

    Application can now host multiple clients

--- batFiles/cheatcardgameServer.bat
+++ batFiles/cheatcardgameServer.bat
@@ -1,1 +1,2 @@
-java -cp .;ocsf.jar;mysql-connector-java-5.1.40-bin.jar;jgoodies-forms-1.8.0-sources.jar;jgoodies-forms-1.8.0.jar cheatcardgame.ServerGUI
+java -cp .;ocsf.jar;mysql-connector-java-5.1.40-bin.jar;jgoodies-forms-1.8.0-sources.jar;jgoodies-forms-1.8.0.jar cheatcardgame.ServerGUI 
+pause
