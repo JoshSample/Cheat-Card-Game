f38e3f5097e4f1c588d5305da0b9b3df8f4874f8
JoshSample <joshsample98@gmail.com>
2020-12-01 09:09:03 -0600

    Updated bat files

--- CheatCardGame/cheatcardgameClient.bat
+++ CheatCardGame/cheatcardgameClient.bat
@@ -1,1 +1,1 @@
-java -cp .;ocsf.jar;mysql-connector-java-5.1.40-bin.jar cheatcardgame.ClientGUI
+java -cp .;ocsf.jar;mysql-connector-java-5.1.40-bin.jar;jgoodies-forms-1.8.0-sources.jar;jgoodies-forms-1.8.0.jar cheatcardgame.ClientGUI
--- CheatCardGame/cheatcardgameServer.bat
+++ CheatCardGame/cheatcardgameServer.bat
@@ -1,1 +1,1 @@
-java -cp .;ocsf.jar;mysql-connector-java-5.1.40-bin.jar cheatcardgame.ServerGUI
+java -cp .;ocsf.jar;mysql-connector-java-5.1.40-bin.jar;jgoodies-forms-1.8.0-sources.jar;jgoodies-forms-1.8.0.jar cheatcardgame.ServerGUI
